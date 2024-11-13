055d89434c017397a584cf701d7bf03d96eacd73
CapPT04 <phatasdasd0198@gmail.com>
2024-11-13 15:09:01 +0700

    Fish many errors and update func
    update checkout func and fix some view error in method auction.

--- src/axios/UserService.js
+++ src/axios/UserService.js
@@ -16,6 +16,8 @@ const END_POINT = {
   GETFEE: "Fee/GetFee",
   UPDATEFEE: "Fee/UpdateFee",
 
+
+
   //breeder
   ENROLLMENTHISTORY: "Enrollment/GetAllEnrollmentsByFishEntryId",
   CREATEREQUEST: "Request/CreateRequest",
@@ -82,6 +84,8 @@ const END_POINT = {
   RECHARGEPAYMENT: "Payment/RechargePayment",
   RECHARGEPAYMENTCALLBACK: "Payment/RechargePaymentCallBack",
   TRANSACTIONHISTORY: "Transaction/GetTransactionHistory",
+  GETPAYMENTPRICE: "FishEntry/GetPaymentPrice",
+
   //Payment
   FEEWALLETPAYMENT: "Payment/FeePayment",
 };
@@ -364,15 +368,11 @@ export const handleGetUnpaidBiddingHistoryByMemberIdApi = async (id) => {
 };
 
 export const handleWinnerPaymentApi = async (
-  checkoutData,
   token,
   fishEntryId
 ) => {
   try {
     return await axiosClient.post(`${END_POINT.WINNERPAYMENT}`, {
-      phone: checkoutData.phone,
-      address: checkoutData.address,
-      city: checkoutData.city,
       token: token,
       fishEntryId: fishEntryId,
     });
@@ -507,6 +507,14 @@ export const handleSubmitRequest = async (request) => {
       expectedDate: request.expectedDate,
     });
     return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const handleGetPaymentPriceApi = async (fishEntryId) => {
+  try {
+    return await axiosClient.get(`${END_POINT.GETPAYMENTPRICE}?f=${fishEntryId}`);
   } catch (error) {
     throw error;
   }
